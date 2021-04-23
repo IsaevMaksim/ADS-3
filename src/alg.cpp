40eeede674fead7f66331f008080af00accab041
IsaevMaksim <78897852+IsaevMaksim@users.noreply.github.com>
2021-04-23 17:57:36 +0300

    isaev

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,12 +2,73 @@
 #include <string>
 #include "tstack.h"
 
+int pri(char c) {
+  if (c == '*' || c == '/')
+    return 3;
+  else if (c == '+' || c == '-')
+    return 2;
+  else if (c == ')')
+    return 1;
+  else
+    return 0;
+}
+
 std::string infx2pstfx(std::string inf) {
-  // добавьте сюда нужный код
-  return std::string("");
+  TStack<char> stack;
+  std::string str;
+  for (int i = 0; i < inf.length(); i++) {
+    if (inf[i] >= '1' && inf[i] <= '9') {
+      str += inf[i];
+      str += ' ';
+    }
+    else if (inf[i] == '(' || pri(inf[i]) > pri(stack.get()) || stack.isEmpty())
+      stack.push(inf[i]);
+    else if (inf[i] == ')') {
+      char ch;
+      do {
+        ch = stack.get();
+        stack.pop();
+        str += ch;
+        str += ' ';
+      } while (ch != '(');
+    }
+    else {
+      while (pri(stack.get()) >= pri(inf[i]) && !stack.isEmpty()) {
+        str += stack.get();
+        str += ' ';
+        stack.pop();
+      }
+      stack.push(inf[i]);
+    }
+  }
+  while (!stack.isEmpty()) {
+    str += stack.get();
+    str += ' ';
+    stack.pop();
+  }
+  str.pop_back();
+  return str;
 }
 
 int eval(std::string pst) {
-  // добавьте сюда нужный код
-  return 0;
+  TStack<char> stack;
+  for (int i=0; i<pst.length(); i++){
+    if (pst[i] >= '1' && pst[i] <= '9')
+      stack.push(pst[i] - '0');
+    else if (pst[i] != ' ') {
+      int n2 = stack.get();
+      stack.pop();
+      int n1 = stack.get();
+      stack.pop();
+      if (pst[i] == '*')
+        stack.push(n1 * n2);
+      else if (pst[i] == '+')
+        stack.push(n1 + n2);
+      else if (pst[i] == '/')
+        stack.push(n1 / n2);
+      else
+        stack.push(n1 - n2);
+    }
+  }
+  return stack.get();
 }
